452003bf36f8e2680c89179c9e394e97f401db0e
Juan Paredes <juanjoseparedes@hotmail.com>
2022-12-10 15:01:48 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { MainLayout } from '../components/layouts/MainLayout'
 export default function HomePage() {
   return (
     <MainLayout>
-      <h1>Home page</h1>
+      <h1>Home Page</h1>
 
        <h1 className={"title"}>
           Ir a <Link href="/">Home</Link>
